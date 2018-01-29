b72da2b50de22f143eacbb0e2fa644ba0e419341
ZEE.KIM <zee.kim@qq.com>
2018-01-29 12:49:06 +0800

    fixed db file error

--- core/db.php
+++ core/db.php
@@ -1,15 +1,12 @@
 <?php
 class db {
 	public $link;
-
-<<<<<<< HEAD
 	function __construct($type = DB_TYPE, $db = DB_NAME, $password = DB_PASSWORD, $user = DB_USER, $host = DB_HOST, $port = DB_PORT, $charset = 'utf8') {
 		try {
 			switch ($type) {
 			case 'sqlite':
 				$this->link = new PDO("sqlite:" . $db);
 				break;
-
 			default:
 				$this->link = new PDO("mysql:host=$host;dbname=$db;port=$port;charset=$charset", $user, $password, array(
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //默认是PDO::ERRMODE_SILENT, 0, (忽略错误模式)
@@ -17,43 +14,29 @@ class db {
 				));
 				break;
 			}
-
-=======
-	function __construct($dbname, $password = '123456', $user = 'root', $host = '127.0.0.1', $port = 3306, $charset = 'utf8') {
-		try {
-			$this->link = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=$charset", $user, $password, array(
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //默认是PDO::ERRMODE_SILENT, 0, (忽略错误模式)
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // 默认是PDO::FETCH_BOTH, 4
-			));
->>>>>>> parent of 93f799c... sqlite add
 		} catch (PDOException $e) {
 			die($e->getMessage());
 		}
 		//$this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    //设置异常处理方式
 		//$this->link->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);   //设置默认关联索引遍历
 	}
-
 	public static function create($type = DB_TYPE, $db = DB_NAME, $password = DB_PASSWORD, $user = DB_USER, $host = DB_HOST, $port = DB_PORT, $charset = 'utf8') {
 		$p = new PDO("mysql:host=$host;port=$port;charset=$charset", $user, $password);
 		$p->query("CREATE DATABASE $db");
 		return new db($type, $db, $password, $user, $host, $port, $charset);
 	}
-
 	function row($sql, $mode = PDO::FETCH_ASSOC) {
 		$stmt = $this->link->query($sql);
 		return $stmt->fetch($mode);
 	}
-
 	function rows($sql, $mode = PDO::FETCH_ASSOC) {
 		$stmt = $this->link->query($sql);
 		return $stmt->fetchAll($mode);
 	}
-
 	function count($sql) {
 		$stmt = $this->link->query($sql);
 		return $stmt->rowCount();
 	}
-
 	function insert($table, $values) {
 		$ks = '';
 		$vs = '';
@@ -74,7 +57,6 @@ class db {
 			return $e->getMessage();
 		}
 	}
-
 	function update($table, $values, $condition = '1=1') {
 		$v = '';
 		if (is_string($values)) {
@@ -96,7 +78,6 @@ class db {
 			return $e->getMessage();
 		}
 	}
-
 	function delete($table, $condition = '') {
 		if (empty($condition) || $condition == '') {
 			$sql = "delete from $table";
@@ -114,25 +95,20 @@ class db {
 			return $e->getMessage();
 		}
 	}
-
 	function exec($sql) {
 		return $this->link->exec($sql);
 	}
-
 	function id() {
 		return $this->link->lastInsertId();
 	}
-
 	function clear($tables = array()) {
 		if (empty($tables)) {
 			return false;
 		}
-
 		foreach ($tables as $key => $table) {
 			$this->exec("TRUNCATE TABLE `$table`");
 		}
 	}
-
 	function export() {
 		$tables = [];
 		$tables = $this->rows("SHOW TABLES", PDO::FETCH_NUM);
@@ -141,7 +117,6 @@ class db {
 		foreach ($tables as $v) {
 			$sql .= "DROP TABLE IF EXISTS `$v`;\n";
 			$rs = $this->row("show create table $v");
-
 			$sql .= $rs["Create Table"] . ";\n\n";
 		}
 		foreach ($tables as $v) {
