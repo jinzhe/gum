130eae0fd603015a80649776db1464d241f04f77
ZEE.KIM <zee.kim@qq.com>
2019-05-03 10:55:18 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,7 +4,7 @@ require "./core/gum.php";
 header("Content-type:text/xml");
 
 $db   = new db();
-$sql  = "SELECT id,time,title,content FROM post WHERE status=1 ORDER BY id DESC LIMIT 20";
+$sql  = "SELECT * FROM post WHERE status=1 ORDER BY id DESC LIMIT 20";
 $rows = $db->rows($sql);
 if (isset($_SERVER['HTTPS']) &&
     ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
@@ -27,6 +27,12 @@ foreach ($rows as $row) {
     $xml .= "\t<link>" . $protocol . $_SERVER['HTTP_HOST'] . "/post/" . $row['id'] . ".html</link>\r\n";
     $xml .= "\t<pubDate>" . date('D, d M Y H:i:s O', $row['time']) . "</pubDate>\r\n";
     $xml .= "\t<description><![CDATA[ " . $row['content'] . " ]]></description>\r\n";
+
+    $xml .= "\t<category><![CDATA[ " . $row['tag_name'] . " ]]></category>\r\n";
+    if (!empty($row['author'])) {
+        $xml .= "\t<author><![CDATA[ " . $row['author'] . " ]]></author>\r\n";
+    }
+
     $xml .= "\t<title><![CDATA[ " . $row['title'] . " ]]></title>\r\n";
     $xml .= "</item>\r\n";
 
