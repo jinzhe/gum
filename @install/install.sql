820f84b76cfd3622719303c9a54cb8bddb91def2
jinzhe <zee.kim@qq.com>
2019-07-31 16:04:14 +0800

    Update install.sql

--- @install/install.sql
+++ @install/install.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS `config`;
-
 CREATE TABLE `config` (
   `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0:文本，1:富文本，2:上传文件，4:开关',
   `key` varchar(50) NOT NULL DEFAULT '' COMMENT '变量',
@@ -8,7 +7,6 @@ CREATE TABLE `config` (
   `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态',
   PRIMARY KEY (`key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 INSERT INTO `config` (`type`, `key`, `value`, `description`, `status`)
 VALUES
 	(0,'title','幸福彼岸','标题',1),
@@ -18,9 +16,7 @@ VALUES
 	(0,'copyright','ZEE.KIM','版权',1),
 	(2,'about-avatar','','关于头像',1),
 	(1,'about-content','','关于内容',1);
-
 DROP TABLE IF EXISTS `link`;
-
 CREATE TABLE `link` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
@@ -33,11 +29,8 @@ CREATE TABLE `link` (
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 INSERT INTO `link` (`id`, `title`, `image`,`image_id`, `url`, `tag`, `sort`, `time`, `status`) VALUES(1,'小林子','',0,'https://xlzi.cn','link',1,2147483647,1),(2,'傻瓜','',0,'http://shagua.name','link',2,1544092212,1),(3,'幸福彼岸','','http://zee.kim','link',0,1555002780,1);
-
 DROP TABLE IF EXISTS `post`;
-
 CREATE TABLE `post` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
@@ -56,9 +49,7 @@ CREATE TABLE `post` (
   `tag_name` varchar(20) NOT NULL DEFAULT '' COMMENT '标签名字',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 DROP TABLE IF EXISTS `tag`;
-
 CREATE TABLE `tag` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8 NOT NULL DEFAULT '',
@@ -69,11 +60,8 @@ CREATE TABLE `tag` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 INSERT INTO `tag` (`id`, `name`, `count`, `tag`, `sort`, `status`) VALUES(1,'笔记',0,'',0,1),(2,'其他',0,'',1,1);
-
 DROP TABLE IF EXISTS `upload`;
-
 CREATE TABLE `upload` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(100) NOT NULL DEFAULT '' COMMENT '服务相对文件路径',
@@ -85,9 +73,7 @@ CREATE TABLE `upload` (
   `bind_id` varchar(50) NOT NULL DEFAULT '0' COMMENT '上传服务关联id',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 DROP TABLE IF EXISTS `user`;
-
 CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `account` varchar(20) NOT NULL DEFAULT '' COMMENT '自定义账号',
@@ -108,11 +94,7 @@ CREATE TABLE `user` (
   `token` varchar(100) NOT NULL DEFAULT '' COMMENT '令牌',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-
 DROP TABLE IF EXISTS `user_code`;
-
 CREATE TABLE `user_code` (
   `type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0:EMAIL,1:TEL',
   `code` varchar(100) NOT NULL DEFAULT '' COMMENT 'email认证码或者短信验证码',
@@ -120,9 +102,7 @@ CREATE TABLE `user_code` (
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否验证',
   `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '关联用户id'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 DROP TABLE IF EXISTS `comment`;
-
 CREATE TABLE `comment` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `nickname` varchar(50) NOT NULL DEFAULT '' COMMENT '昵称',
@@ -135,6 +115,6 @@ CREATE TABLE `comment` (
   `bind_type` varchar(20) NOT NULL DEFAULT '' COMMENT '评论服务类型',
   `bind_value` varchar(255) NOT NULL DEFAULT '' COMMENT '评论服务关联值',
   `parent_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '父评论id',
-  `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户id',
+  `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
